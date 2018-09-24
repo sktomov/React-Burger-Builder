ca67ee7f2a667be77651309930af62ca466fc178
le6o <sktomov@gmail.com>
2018-09-24 11:34:12 +0300

    adding sidedrawer

--- src/components/Layouts/Layout.js
+++ src/components/Layouts/Layout.js
@@ -1,17 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Auxx from '../../hoc/Auxx';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-    <Auxx>
-        <Toolbar />
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Auxx>  
-);
+class Layout extends Component {
+    state = {
+        showSideDrawer: true
+    }
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+    render() {
+        return (
+            <Auxx>
+                <Toolbar />
+                <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+        </Auxx>  
+        )
+    }
+};
+    
 
-export default layout;
+export default Layout;
